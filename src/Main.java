e45421d3f78b5cdacce113cb7ec04ea886477264
JSQdev <148073798+devJSQ@users.noreply.github.com>
2023-12-17 21:53:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 class Main {
   public static void main(String[] args) {
-     /* your code here */
+     Bank dflt = new Bank();
+     SNB snb = new SNB();
+     SAB sab = new SAB();
+
+      System.out.println("Interest Rate of normal Bank: " + dflt.getRateOfInterest() + "%");
+      System.out.println("the intrst Rate of SNB: " + snb.getRateOfInterest() + "%");
+      System.out.println("the intrest Rate of SAB: " + sab.getRateOfInterest() + "%");
   }
+}
+
+class Bank {
+    public double getRateOfInterest() {
+        return 0.0;
+    }
+}
+
+class SNB extends Bank{
+  public double getRateOfInterest() {
+        return 8.2; 
+    }
+}
+
+class SAB extends Bank{
+  public double getRateOfInterest() {
+        return 2.9; 
+    }
 }
